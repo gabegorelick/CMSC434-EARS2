f0efedfd389c3327bc04453567396aea0c1bc159
Alisa Chen <ddigitalz@gmail.com>
2011-11-16 17:59:50 -0500

    Attempting to resolve float problems

--- WebContent/Project 3/forStudents/js/resize.js
+++ WebContent/Project 3/forStudents/js/resize.js
@@ -5,21 +5,17 @@ function checkAvailableWidth() {
 	var bodyWidth;
 	var helpWidth
 	
-	if (numClientWidth  > 1020) {
+	if (numClientWidth  > 1015) {
 		bodyWidth = "1015px";
 		helpWidth = "943px";
 		$("#header").css("text-align", "center");
 		$("#head_img").css("margin-left", "30px");
 		$("#help_panel h2").css("text-align", "center");
 
-		$("#filter_panel").css("float", "left");
-		$("#filter_panel").css("position", "relative");
-
 		$("#container_right").css("float", "right");
 		$("#chart_panel h2").css("text-align", "center");
 		$("#chart_panel h2").css("margin-left", "0px");
 		$("#map").css("margin-left", "0px");
-		$("#map").css("margin-right", "0px");
 	} else {
 		bodyWidth = "500px";
 		helpWidth = "";
@@ -27,15 +23,11 @@ function checkAvailableWidth() {
 		$("#head_img").css("margin-left", "0px");
 		$("#help_panel h2").css("text-align", "left");
 
-		$("#filter_panel").css("float", "none");
-		$("#filter_panel").css("position", "fixed");
-
 		$("#container_right").css("float", "left");
 		$("#chart_panel h2").css("text-align", "left");
 		$("#chart_panel h2").css("margin-left", "30px");
 		$("#map").css("margin-left", "5px");
-		$("#map").css("margin-right", "auto");
 	}
 	$("#container").css("width", bodyWidth);
-	$("#help_panel").css("width", helpWidth);
+	//help_panel").css("width", helpWidth);
 }
